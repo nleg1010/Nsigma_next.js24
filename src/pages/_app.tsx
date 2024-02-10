7a230eb4bd3e80caead827b6c202df3e46591af2
nleg1010 <155825822+nleg1010@users.noreply.github.com>
2024-02-10 12:48:48 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,20 @@
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Global meta tags */}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp;
+
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Footer from "@/components/footer";
