c2958f2397eec2783fc27326a03954c43ef10e7a
cristvilla <ednoguer00@gmail.com>
2024-02-19 03:48:10 -0500

    update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import Footer from "@/components/footer";
 import Header from "@/components/Header/header";
 import { Source_Sans_3 } from "next/font/google";
@@ -8,24 +7,17 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-//  const source_sans = Source_Sans_3({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   weight: "400"
-// })
-
 const source_sans = Source_Sans_3({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { pathname } = useRouter();
   return (
     <main className={`${source_sans.variable} font-sans`}>
-      {!pathname.includes("/studio") && <Header />}
+      <Header />
       <Component {...pageProps} />
-      {!pathname.includes("/studio") && <Footer />}
+      <Footer />
     </main>
   );
 }
