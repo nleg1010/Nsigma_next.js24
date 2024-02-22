3313fd1b67c7eef1a28e9e9fd1adee297b7e4f49
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-22 14:42:04 +0300

    fixed part 3 sloppy animation

--- src/components/engagement-strategies/EngagementCard.tsx
+++ src/components/engagement-strategies/EngagementCard.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 type EngagementCardProps = {
   image: { url: string };
   info: string;
@@ -5,25 +7,59 @@ type EngagementCardProps = {
 };
 
 const EngagementCard = ({ image, info, title }: EngagementCardProps) => {
+  const [loading, setLoading] = useState(true);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    // Simulate data fetching delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 7000); // Adjust the delay time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="w-full sm:h-[400px] xl:w-[400px] bg-transparent cursor-pointer group  lg:perspective-1000">
-      <div className="relative w-full h-full lg:preserve-3d  lg:group-hover:rotate-y-180  duration-500">
-        <img
-          src={image?.url}
-          alt={title}
-          className="hidden lg:block p-1 rounded-2xl absolute w-full h-full top-0 left-0 right-0 bottom-0"
-        />
-        <div className="font-bold px-4 text-3xl bottom-4 hidden lg:block absolute text-neutral-300">
-          {title}
+    <div
+      className={`w-full sm:h-[400px] xl:w-[400px] bg-transparent cursor-pointer group  lg:perspective-1000 ${
+        loading ? 'blur' : ''
+      }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {loading ? (
+        <div className="w-full h-full flex items-center justify-center text-neutral-300">
+  	<img
+            src={image?.url}
+            alt={title}
+            className="hidden lg:block p-1 rounded-2xl absolute w-full h-full top-0 left-0 right-0 bottom-0"
+          />
         </div>
-        <div className="sm:absolute rounded-2xl lg:rotate-y-180 w-full h-full flex flex-col items-center justify-center gap-4 py-4 bg-[#272932]  overflow-hidden p-4 xl:p-10 text-neutral-300 lg:backface-hidden">
-          <div className="font-bold text-3xl block lg:hidden text-center">
+      ) : (
+        <div
+          className={`relative w-full h-full lg:preserve-3d duration-1000 ${
+            isHovered ? 'rotate-y-180' : ''
+          }`}
+        >
+          <img
+            src={image?.url}
+            alt={title}
+            className="hidden lg:block p-1 rounded-2xl absolute w-full h-full top-0 left-0 right-0 bottom-0"
+          />
+          <div className="font-bold px-4 text-3xl bottom-4 hidden lg:block absolute text-neutral-300">
             {title}
           </div>
-          <div className="flex flex-col space-y-5">{info}</div>
+          <div className="sm:absolute rounded-2xl lg:rotate-y-180 w-full h-full flex flex-col items-center justify-center gap-4 py-4 bg-[#272932]  overflow-hidden p-4 xl:p-10 text-neutral-300 lg:backface-hidden">
+            <div className="font-bold text-3xl block lg:hidden text-center">
+              {title}
+            </div>
+            <div className="flex flex-col space-y-5">{info}</div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
+
 export default EngagementCard;
+
