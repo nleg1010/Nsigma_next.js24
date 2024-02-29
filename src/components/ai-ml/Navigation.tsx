3db4c77861d4e5f24eef912a3dd8b7c05c20aa0b
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-29 13:53:44 +0300

    typescript error fix 7

--- src/components/ai-ml/Navigation.tsx
+++ src/components/ai-ml/Navigation.tsx
@@ -16,10 +16,7 @@ const SECTIONS = [
   { title: "Why Us", id: "why-us" },
 ];
 
-type NavigationSliceProps =
-  SliceComponentProps<Content.NavigationSliceSlice>;
-
-function Navigation({ slices }: NavigationSliceProps) {
+function Navigation() {
 	const [activeTab, setActiveTab] = useState(0);
 
 	const handleTabClick = (index: any) => {
@@ -28,41 +25,7 @@ function Navigation({ slices }: NavigationSliceProps) {
 	const slice = slices[0];
   return (
   <>
-  { slice &&
-<section
-      className="w-full flex justify-center items-center py-12"
-      id="our-process"
-    >
-      <div className="container flex flex-col gap-12 text-white px-4">
-        <div className="text-l bg-Gray p-4 sm:p-8 md:p-5 rounded-2xl flex flex-col" style={{ height: '0px' }}>
-          <div className="flex pb-8 gap-2 sm:gap-12" style={{ marginTop: '-15px'}}>
-            {slice?.items.map(({ label, link }, i) => (
-	    <>
-              <button
-                key={i}
-                className={`relative focus:outline-none group`}
-                onClick={() => handleTabClick(i)}
-              >
-	      	<Link href={`#${(link as { url: string | undefined })?.url}`}>
-		    <PrismicRichText field={label} />
-		</Link>
-                <span
-                  className={twMerge(
-                    "absolute top-[calc(100%_+_1.75rem)] w-full bg-customGreen h-1 duration-300 opacity-0 transition-opacity left-0 group-hover:opacity-100",
-                    i === activeTab && 0 > 1 &&"opacity-100"
-                  )}
-		  style={{ marginTop: '-17px'}}
-                ></span>
-              </button>
-	      </>
-            ))}
-          </div>
-          <div className="w-full bg-black h-[2px] mb-6"></div>
-        </div>
-      </div>
-    </section>
-    }
-  
+    
     <div className="w-full flex items-center justify-center">
       <div className="container w-fit mx-4 px-4 bg-Gray flex-wrap flex items-center justify-center rounded-[42px] py-2 text-white gap-x-8 gap-y-2">
         {SECTIONS.map(({ title, id }) => (
