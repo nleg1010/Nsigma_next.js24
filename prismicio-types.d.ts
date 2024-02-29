464d57a49ef383fd2bfd3f0f6b358515404ecc7a
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-29 09:33:21 +0300

    what is up ezra

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,7 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type AiMlDocumentDataSlicesSlice = never;
+type AiMlDocumentDataSlicesSlice = NavigationSliceSlice;
 
 /**
  * Item in *AI/ML → AI/Ml Steps*
