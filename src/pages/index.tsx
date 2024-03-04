faee9136144acd4bea5e0ee3f3e9931b2c755d2a
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-03-04 10:01:47 +0300

    addition of scripts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,7 @@ const Home: FC<any> = ({ page }) => {
 
   return (
     <>
+      <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/44973111.js"></script>
       <DefaultSeo title={meta_title} description={meta_description} />
       <div className="bg-[#000200]">
         <Banner data={questions} />
