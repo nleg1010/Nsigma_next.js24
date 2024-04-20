a89e46a789fb87616d915042227f98c1bae4cb2d
MrBasta05 <mr.basta05@gmail.com>
2024-04-20 20:36:16 +0200

    Update Text slice styling and add variation withHr

--- src/slices/Text/index.tsx
+++ src/slices/Text/index.tsx
@@ -9,19 +9,19 @@ const Text = ({ slice }: TextProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="flex flex-col items-center text-black text-lg"
+      className="flex pb-32 flex-col items-center text-white text-lg"
     >
       <div
         className={twMerge(
-          "p-4 md:p-16 md:pt-4 text-center max-w-[1180px]",
+          "p-4 md:px-16 md:pt-4 text-center max-w-[1180px]",
           slice.variation === "boldColored" && "title",
-          slice.variation === "withHr" && "relative"
+          slice.variation === "withHr" && "relative w-1/2"
         )}
       >
         {slice.variation === "withHr" && (
           <>
-            <span className="absolute bg-grad h-1 w-[300px] top-0 -translate-x-1/2 rounded-full bg-gradient-to-r from-[#eb9204] to-[#83e868] via-[#83e868]" />
-            <span className="absolute bg-grad h-1 w-[300px] bottom-0 -translate-x-1/2 rounded-full bg-gradient-to-r from-[#eb9204] to-[#83e868] via-[#83e868]" />
+            <span className="absolute bg-grad h-[2px] w-[300px] top-0 -translate-x-1/2 rounded-full bg-gradient-to-r from-[#eb9204] to-[#83e868] via-[#83e868]" />
+            <span className="absolute bg-grad h-[2px] w-[300px] bottom-0 -translate-x-1/2 rounded-full bg-gradient-to-r from-[#eb9204] to-[#83e868] via-[#83e868]" />
           </>
         )}
         <PrismicRichText field={slice.primary.text} />
