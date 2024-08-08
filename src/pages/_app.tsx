dc00991ba81f9dc8d01a1b0cf2b0ddb8442b670f
cristvilla <ednoguer00@gmail.com>
2024-08-08 09:45:35 -0500

    remove unncessary font import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,15 @@
 import type { AppProps } from "next/app";
 import Footer from "@/components/footer";
 import Header from "@/components/Header/header";
-import { Source_Sans_3 } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@/styles/globals.css";
 
-const source_sans = Source_Sans_3({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <main className={`${source_sans.variable} font-sans`}>
+      <main>
         <Header />
         <Component {...pageProps} />
         <Footer />
