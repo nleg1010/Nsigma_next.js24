a693f548d3a0cd9e30fd83ee37991205ba742416
mihalsabados <sabadosmihal00@gmail.com>
2025-04-08 11:37:10 +0200

    fix: remove routes from prismic config

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -15,10 +15,6 @@ export const repositoryName =
  */
 // TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig["routes"] = [
-  {
-    type: "asset_management",
-    path: "/asset-management",
-  },
   // Examples:
   // {
   // 	type: "homepage",
