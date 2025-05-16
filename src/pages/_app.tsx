078acccfbe8ba48bdfdc620c6a7705d686338e34
Mihal Sabadoš <102969313+mihalsabados@users.noreply.github.com>
2025-05-16 14:20:24 +0200

    feat: add amplitude analytics to project (#40)
    * feat: add amplitude analytics to project
    
    * fix: update footer links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,19 @@ import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
 
+import { initAmplitude } from "@/lib/amplitude";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const Footer = dynamic(() => import("@/components/footer"), {
   ssr: false,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initAmplitude();
+  }, []);
+
   return (
     <>
       <main>
